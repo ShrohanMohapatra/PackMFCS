3b1a3e9c6c33f728821c9c4bc1b26955ca71b691
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2021-06-13 20:57:12 +0530

    Add files via upload

--- disjointSetClass.py
+++ disjointSetClass.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 13 19:53:47 2021
+
+@author: shrohanmohapatra
+"""
+
 class disjointSetNew:
     def __init__(self):
         self.forest = {}
@@ -9,7 +17,9 @@ class disjointSetNew:
             self.forest[x]['rank'] = 0
     def Find(self, x):
         root = self.forest[x]
-        while root['parent']!=root:
+        while self.forest[root['parent']]!=self.forest[root]:
+            # This needs to be debugged ASAP ....
+            print('root->',root)
             root = self.forest[root['parent']]
         while self.forest[x]['parent']!= root:
             parent = self.forest[x]['parent']
@@ -17,6 +27,56 @@ class disjointSetNew:
             self.forest[x] = parent
         return root
     def UnionBySize(self,x,y):
-        x = self.Find(self,x)
-        y = self.Find(self,y)
-    # To be written by union by size or union by root ....
+        xRoot = self.Find(x)
+        yRoot = self.Find(y)
+        self.forest[x] = self.forest[xRoot]
+        self.forest[y] = self.forest[yRoot]
+        if self.forest[x] == self.forest[y]: return
+        if self.forest[x]['size'] < self.forest[y]['size']:
+            (self.forest[x],self.forest[y]) = (self.forest[y], \
+                                               self.forest[x])
+        self.forest[y]['parent'] = self.forest[x]
+        self.forest[x]['size'] = self.forest[x]['size'] + \
+            self.forest[y]['size']
+    def UnionByRank(self,x,y):
+        xRoot = self.Find(x)
+        yRoot = self.Find(y)
+        self.forest[x] = self.forest[xRoot]
+        self.forest[y] = self.forest[yRoot]
+        if self.forest[x] == self.forest[y]: return
+        if self.forest[x]['rank'] < self.forest[y]['rank']:
+            (self.forest[x],self.forest[y]) = (self.forest[y], \
+                                               self.forest[x])
+        self.forest[y]['parent'] = self.forest[x]
+        self.forest[x]['rank'] = self.forest[x]['rank'] + 1
+
+# Some elementary basic checks whether they work with the code
+# that works for the class definition
+
+print('Some elementary basic checks ...')
+A = {1:'a',2:'b',3:'c'}
+B = {1:'a',2:'b',3:'d'}
+print(A==B)
+B = A
+print(A==B)
+a = 4
+b = 6
+print(a,b)
+(a, b) = (b, a)
+print(a,b)
+
+print('Now let us test the disjoint set data structure ...')
+newMarrion = disjointSetNew()
+for k in range(1,11):
+    print('Adding k =',k,' to the set!')
+    newMarrion.MakeSet(k)
+for k in range(1,5):
+    print('Unifying',2*k-1,'and',2*k)
+    newMarrion.UnionBySize(2*k-1, 2*k)
+from time import time
+for k in range(1,11):
+    start = time()
+    s1 = newMarrion.Find(k)
+    end = time()
+    print('Finding ',k,'and found',s1)
+    print('Time taken = ',(end-start)*10**6,'usecs')
